06d12d69058f9e0414589bf156db3062533e6709
VurhD <benegaldhruv2312@gmail.com>
2023-08-10 19:00:19 +0800

    Hello World GetMapping added

--- src/main/java/com/practice/practice/PracticeApplication.java
+++ src/main/java/com/practice/practice/PracticeApplication.java
@@ -2,12 +2,20 @@ package com.practice.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
     }
 
 }
