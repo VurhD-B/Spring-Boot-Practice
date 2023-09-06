b238e84ce39af941f1efcf12a1fb09b4b641b33f
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-09-06 18:10:53 +0800

    Created the repositories for User and Role so that we can set custom SQL query methods

---
+++ src/main/java/com/practice/practice/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.practice.practice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.practice.practice.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+    Optional<Role> findByAuthority(String authority);
+}
